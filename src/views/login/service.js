764c41f5e9bc33cf85d5ff389d5e1e5adf1edc4d
logfun <logofun@163.com>
2022-02-07 23:30:07 +0800

    change the host

--- src/views/login/service.js
+++ src/views/login/service.js
@@ -1,3 +1,5 @@
-import fetch from '@/utils/fetch';
+// import fetch from '@/utils/fetch';
+import fetch from '@/utils/api';
 
-export const userLogin = params => fetch.post('api/testUser/login', params)
+// export const userLogin = params => fetch.post('api/testUser/login', params)
+export const userLogin = params => fetch.post('/login', params)
